e0908364b91dc11299747837a2a84b6aaf23ce4b
Mafinar K <mafinar@gmail.com>
2018-11-25 15:58:24 -0500

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:algorithms_in_dart/algorithms_in_dart.dart' as algorithms_in_dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${algorithms_in_dart.calculate()}!');
+}
---
+++ test/algorithms_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:algorithms_in_dart/algorithms_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
