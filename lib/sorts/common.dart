e3fcbcf2139ad6f28a8fca1d91186a35c9de614b
Mafinar K <mafinar@gmail.com>
2018-11-30 13:10:05 -0500

    Add some utility function for sorting

--- lib/sorts/common.dart
+++ lib/sorts/common.dart
@@ -1,8 +1,64 @@
 typedef bool Comparator<T extends Comparable>(T left, T right);
+
+/// Makes sure left is less than right.
+///
+/// In a sorting algorithm, this function needs to be true if the
+/// sorting needs to return ascending order of lists.
+///
+/// For example, if we see an ascending ordered list- `[1, 2, 3, 5]`
+/// we can see from left to right, all two consequetive elements in
+/// this function would return true, in other words, settings this as
+/// `reduce` function would yield true for a list sorted in ascending
+/// order.
 bool ascendingFn<T extends Comparable>(a, b) => a.compareTo(b) <= 0;
 
+/// Swaps two elements in a [List]
 void swap<T extends Comparable>(List<T> list, int i, int j) {
   T temp = list[i];
   list[i] = list[j];
   list[j] = temp;
+}
+
+/// Checks if the list is sorted according to [compareFn]
+bool isSorted<T extends Comparable>(List<T> list,
+    [Comparator<T> compareFn = ascendingFn]) {
+  var i = 0;
+
+  while (i < list.length - 1 && list.length > 1) {
+    if (!compareFn(list[i], list[i + 1])) return false;
+    ++i;
+  }
+
+  return true;
+}
+
+/// Checks if the list is sorted reversely.
+bool isReverseSorted<T extends Comparable>(List<T> list,
+    [Comparator<T> compareFn = ascendingFn]) {
+  var i = 0;
+
+  while (i < list.length - 1 && list.length > 1) {
+    if (compareFn(list[i], list[i + 1])) return false;
+    i++;
+  }
+
+  return true;
+}
+
+/// Returns the minimum and maximum of a [List] as a [Map]
+///
+/// The minimum and maximum values are stored as "min"
+/// and "max" kets in the [Map] returned.
+Map<String, num> findMinMax(List<num> list) {
+  if (list.isEmpty) return null;
+
+  num min = list[0];
+  num max = list[0];
+
+  for (var item in list) {
+    if (item < min) min = item;
+    if (item > max) max = item;
+  }
+
+  return {"min": min, "max": max};
 }
