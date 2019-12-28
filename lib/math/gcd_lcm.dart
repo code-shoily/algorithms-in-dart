60ea08257a6a48a41731fa4021254faab2aeb84d
Mafinar Khan <mafinar@gmail.com>
2019-12-28 17:52:50 -0500

    Add GCD and LCM for lists of numbers

--- lib/math/gcd_lcm.dart
+++ lib/math/gcd_lcm.dart
@@ -2,6 +2,17 @@ int gcd(int x, int y) {
   return x == 0 ? y : gcd(y % x, x);
 }
 
+int gcd_list(List<int> numbers) {
+  if (numbers.length < 2) throw ArgumentError("Please add two or more numbers");
+  return numbers.reduce((a, b) => gcd(a, b));
+}
+
 int lcm(int x, int y) {
+  if (x * y == 0) throw ArgumentError("No number should be zero");
   return (x * y) ~/ gcd(x, y);
+}
+
+int lcm_list(List<int> numbers) {
+  if (numbers.length < 2) throw ArgumentError("Please add two or more numbers");
+  return numbers.reduce((a, b) => lcm(a, b)); 
 }
--- test/math/gcd_lcd_test.dart
+++ test/math/gcd_lcd_test.dart
@@ -14,6 +14,33 @@ void main() {
   test('Test for LCM', () {
     expect(lcm(15, 20), equals(60));
     expect(lcm(5, 7), equals(35));
-    expect(lcm(4, 6), equals(12));
+    try {
+      lcm(4, 0);
+    } catch (e) {
+      expect(e is ArgumentError, isTrue);
+    }
+  });
+
+  test('Test for LCM (List)', () {
+    try {
+      gcd_list([5]);
+    } catch (e) {
+      expect(e is ArgumentError, isTrue);
+    }
+    expect(gcd_list([5, 7]), equals(1));
+    expect(gcd_list([4, 6]), equals(2));
+    expect(gcd_list([4, 6, 0]), equals(2));
+    expect(gcd_list([15, 20, 25, 30, 35]), equals(5));
+    expect(gcd_list([0, 6, 9, 0, 12]), equals(3));
+    expect(gcd_list([0, 0, 0, 0, 0]), equals(0));
+  });
+  test('Test for LCM (List)', () {
+    try {
+      lcm_list([4]);
+    } catch (e) {
+      expect(e is ArgumentError, isTrue);
+    }
+    expect(lcm_list([15, 20, 25, 30, 35]), equals(2100));
+    expect(lcm_list([5, 7, 2]), equals(70));
   });
 }
