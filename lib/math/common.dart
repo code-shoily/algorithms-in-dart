47b096bc74c3c8353b06d926329c6d267d386dce
Mafinar Khan <mafinar@gmail.com>
2019-12-28 18:43:23 -0500

    Add factorial function

--- lib/math/common.dart
+++ lib/math/common.dart
@@ -1,3 +1,5 @@
+import 'package:algorithms_in_dart/helpers/range.dart';
+
 /// Compute Greatest Common Divisor of two numbers.
 int gcd(int x, int y) {
   return x == 0 ? y : gcd(y % x, x);
@@ -6,19 +8,25 @@ int gcd(int x, int y) {
 /// Compute Greatest Common Divisor of a list of numbers.
 /// List must contain two or more more numbers.
 int gcd_list(List<int> numbers) {
-  if (numbers.length < 2) throw ArgumentError("Please add two or more numbers");
+  if (numbers.length < 2) throw ArgumentError("Single element array");
   return numbers.reduce((a, b) => gcd(a, b));
 }
 
 /// Least Common Multiple of two non-zero numbers.
 int lcm(int x, int y) {
-  if (x * y == 0) throw ArgumentError("No number should be zero");
+  if (x * y == 0) throw ArgumentError("Zero input");
   return (x * y) ~/ gcd(x, y);
 }
 
 /// Least Common Multiple of a list of numbers.
 /// List must contain two or more non-zero numbers.
 int lcm_list(List<int> numbers) {
-  if (numbers.length < 2) throw ArgumentError("Please add two or more numbers");
+  if (numbers.length < 2) throw ArgumentError("Single element array");
   return numbers.reduce((a, b) => lcm(a, b)); 
+}
+
+/// Factorial of n. 
+int factorial(int n) {
+  if (n < 0) throw ArgumentError("Negative Number");
+  return (n <= 1) ? 1 : 2.to(n + 1).reduce((a, b) => a * b);
 }
--- test/math/common_test.dart
+++ test/math/common_test.dart
@@ -1,6 +1,6 @@
 import 'package:test/test.dart';
 
-import 'package:algorithms_in_dart/math/gcd_lcm.dart';
+import 'package:algorithms_in_dart/math/common.dart';
 
 void main() {
   test('Test for LCM', () {
@@ -42,5 +42,20 @@ void main() {
     }
     expect(lcm_list([15, 20, 25, 30, 35]), equals(2100));
     expect(lcm_list([5, 7, 2]), equals(70));
+  });
+
+  test('Factorial', () {
+    expect(factorial(0), equals(1));
+    expect(factorial(1), equals(1));
+    expect(factorial(2), equals(2));
+    expect(factorial(3), equals(6));
+    expect(factorial(4), equals(24));
+    expect(factorial(5), equals(120));
+
+    try {
+      factorial(-2);
+    } catch (e) {
+      expect(e is ArgumentError, isTrue);
+    }
   });
 }
