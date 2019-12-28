4200dc4fff3db98e4815b3a96d734d3bda3b970e
Mafinar Khan <mafinar@gmail.com>
2019-12-28 18:52:34 -0500

    Add the range helper

---
+++ lib/helpers/range.dart
@@ -0,0 +1,13 @@
+extension NumRange on int {
+  /// Returns a range of `[this, end)`. Counts up and down both.
+  ///
+  /// Usage:
+  ///   `1.to(10)` for a list of numbers from 1 to 10 (10 excluded)
+  ///   `10.to(1)` for a list of numbers from 10 to 1 (1 excluded)
+  List<int> to(int end) => [
+        for (var n = this;
+            (this > end) ? n > end : n < end;
+            (this > end) ? n-- : n++)
+          n
+      ];
+}
---
+++ test/helpers/range_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+
+import 'package:algorithms_in_dart/helpers/range.dart';
+
+void main() {
+  test('Test ranges', () {
+    expect(1.to(1), equals(<int>[]));
+    expect(10.to(15), equals(<int>[10, 11, 12, 13, 14]));
+    expect((-1).to(-5), equals(<int>[-1, -2, -3, -4]));
+    expect(5.to(1), equals(<int>[5, 4, 3, 2]));
+  });
+}
