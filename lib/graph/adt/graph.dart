6d7e441fc1975729fd4f15179406febe99497c9a
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-10-28 02:09:06 -0400

    Refactor graph.dart

--- lib/graph/adt/graph.dart
+++ lib/graph/adt/graph.dart
@@ -1,7 +1,7 @@
 import 'dart:collection';
 
 import './settings.dart';
-import 'node.dart';
+import './vertex.dart';
 
 /// A Graph Type
 class Graph<T> {
@@ -20,17 +20,45 @@ class Graph<T> {
   /// Total number of vertices for this graph
   int get numberOfVertices => vertices.entries.length;
 
+  /// Total number of edges
+  int get numberOfEdges =>
+      vertices.values.map((v) => v.outDegree).fold(0, (a, b) => a + b);
+
   /// Adds an edge
   void addEdge(Vertex src, Vertex dst, [int weight = 1]) {
     if (src.key == dst.key && !settings.allowLoops) throw Error();
 
-    src = _getOrAddVertex(src.key, src.value);
-    dst = _getOrAddVertex(dst.key, dst.value);
+    src = _getOrAddVertex(src);
+    dst = _getOrAddVertex(dst);
     src.addConnection(dst, weight);
 
     if (!settings.isDigraph) dst.addConnection(src, weight);
   }
 
-  Vertex<T> _getOrAddVertex(String key, [T value]) =>
-      vertices.putIfAbsent(key, () => Vertex(key, value ?? key));
+  /// Checks if vertex is included
+  bool containsVertex(Vertex vertex) => vertices[vertex.key] == vertex;
+
+  /// Checks if vertex is included by key
+  bool containsVertexKey(String vertexKey) => vertices.containsKey(vertexKey);
+
+  /// Checks if this is a null graph
+  bool get isNull => numberOfVertices == 0;
+
+  /// Checks if this is a singleton graph
+  bool get isSingleton => numberOfVertices == 1;
+
+  /// Checks if this is an empty graph
+  bool get isEmpty => numberOfEdges == 0;
+
+  /// Adds a new vertex
+  bool addVertex(Vertex vertex) {
+    if (containsVertexKey(vertex.key)) {
+      return false;
+    }
+    vertices[vertex.key] = vertex;
+    return true;
+  }
+
+  Vertex<T> _getOrAddVertex(Vertex vertex) =>
+      vertices.putIfAbsent(vertex.key, () => vertex);
 }
